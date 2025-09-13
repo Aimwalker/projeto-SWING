b7547a23bde1b68f1fc8236b5a953322f1368b63
guilherme <guimiranda199@gmail.com>
2025-09-12 22:30:25 -0300

    add

--- src/main/java/com/br/pdvpostocombustivel/PdvpostocombustivelApplication.java
+++ src/main/java/com/br/pdvpostocombustivel/PdvpostocombustivelApplication.java
@@ -7,7 +7,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PdvpostocombustivelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PdvpostocombustivelApplication.class, args);
+		//SpringApplication.run(PdvpostocombustivelApplication.class, args);
 	}
-
+    Pessoa pessoa1 = new Pessoa();
+    pessoa1.setApelido();
+    pessoa1.setId();
+    pessoa1.setIdade();
+    pessoa1.setNome();
+    rp.salvaPessoa();
+    Pessoa pessoa2 = new Pessoa();
+    pessoa1.setApelido();
+    pessoa1.setId();
+    pessoa1.setIdade();
+    pessoa1.setNome();
+    rp.salvaPessoa();
+    Pessoa pessoa3 = new Pessoa();
+    pessoa1.setApelido();
+    pessoa1.setId();
+    pessoa1.setIdade();
+    pessoa1.setNome();
+    rp.salvaPessoa();
 }
