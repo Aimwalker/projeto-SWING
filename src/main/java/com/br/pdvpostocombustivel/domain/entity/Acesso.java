4aaea70b838dfe5f26093e706fe52af82bc5a450
Aimwalker <Guimiranda199@gmail.com>
2025-09-14 19:23:58 -0300

    att

---
+++ src/main/java/com/br/pdvpostocombustivel/domain/entity/Acesso.java
@@ -0,0 +1,27 @@
+package com.br.pdvpostocombustivel.domain.entity;
+
+public class Acesso {
+    private String login;
+    private String senha;
+
+    public Acesso(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
